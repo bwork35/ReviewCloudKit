1e2250bc1e167140922b2acede51c1d491df2dfb
Bryan Workman <bryan@bryans-mbp.lan>
2021-01-30 22:22:32 -0800

    initial Journal project and set up initial files

---
+++ JournalCK00/JournalCK00/Controllers/ViewControlles/EntryListTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EntryListTableViewController.swift
+//  JournalCK00
+//
+//  Created by Bryan Workman on 1/30/21.
+//
+
+import UIKit
+
+class EntryListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+} //End of class
